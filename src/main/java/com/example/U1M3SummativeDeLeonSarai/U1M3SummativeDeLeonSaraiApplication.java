b45930fd50039ea13d77c0c997550c86c42915ef
Sarai De Leon <saraideleon@live.com>
2019-07-18 14:28:15 -0500

    first commit

---
+++ src/main/java/com/example/U1M3SummativeDeLeonSarai/U1M3SummativeDeLeonSaraiApplication.java
@@ -0,0 +1,13 @@
+package com.example.U1M3SummativeDeLeonSarai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M3SummativeDeLeonSaraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M3SummativeDeLeonSaraiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/U1M3SummativeDeLeonSarai/U1M3SummativeDeLeonSaraiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.U1M3SummativeDeLeonSarai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class U1M3SummativeDeLeonSaraiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
